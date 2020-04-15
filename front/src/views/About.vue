ee05c1c9b87b733cbcfc8671300fdddd338e5f5e
Alibriaan <dimulka62@gmail.com>
2020-04-15 23:27:53 +0300

    add(About.vue):
    layout

---
+++ front/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+  <div class="posts-item" v-for="(item,index) in posts" :key="index">
+    <h1>{{item.title}}</h1>
+    <p>{{item.description}}</p>
+  </div>
+  <input type="text" placeholder="title" v-model="title" />
+  <input type="text" placeholder="description" v-model="description" />
+  <button @click="sendDataOnServer">Send</Button>
+  </div>
+</template>
