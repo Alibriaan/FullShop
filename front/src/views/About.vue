3b7c24ae1d68694e1bd386260fbf8306ecaf4279
Alibriaan <dimulka62@gmail.com>
2020-04-15 23:31:53 +0300

    update(About):
    add logic and css

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -9,3 +9,41 @@
   <button @click="sendDataOnServer">Send</Button>
   </div>
 </template>
+
+<script>
+// import PostsRequset from '../services/postRequest.js';
+// import PostsService from '../services/postsService.js';
+export default {
+  name: 'about',
+  data: () => {
+    return {
+      posts: [],
+      title: "",
+      description: ""
+    }
+  },
+  
+  methods: {
+    async sendDataOnServer() {
+      console.log("Post"); 
+      // await PostsRequset.fetchPosts({
+      //   title: this.title,
+      //   description: this.description
+      // });
+      // const response = await PostsService.fetchPosts()
+      // console.log(response.data);
+    },
+    async getPosts() {
+        console.log("Get запрос");
+        // const response = await PostsService.fetchPosts()
+        // this.posts = response.data;
+    }
+  },
+  mounted() {
+    // this.getPosts();
+  }
+}
+</script>
+
+<style scoped>
+</style>
