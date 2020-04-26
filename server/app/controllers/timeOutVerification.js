1e994488f4896e1c78f67bd7a358fdd2f521aa24
Alibriaan <dimulka62@gmail.com>
2020-04-26 12:38:25 +0300

    refactoring( timeOutVerification controller):

--- server/app/controllers/timeOutVerification.js
+++ server/app/controllers/timeOutVerification.js
@@ -1,21 +1,25 @@
-const bCrypt = require("bcrypt");
+//const bCrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const moment = require("moment");
 const config = require("../../config/app");
 const mailer = require("../../config/nodemailer");
 
+const ERR_MAIL_DOESNT_SEND = "ERR_MAIL_DOESNT_SEND";
+const EMAIL_SUCCESS = "EMAIL_SUCCESS";
 
 const timeOutVerification = (req, res) => {
-
     const tokenLifeStart = moment();
-    const verificationToken  = req.body.token;
-    const email = jwt.decode(verificationToken).email;
+    const email = jwt.decode(req.body.token).email;
     const activationLink = config.activationLink + jwt.sign({ 
         email,
         tokenLifeStart
     }, config.bcryptSecretKey);
+    const mailConfig = initEmailList(email, activationLink);
+    sendList(mailConfig);
+}
 
-    const mailConfig = {
+function initEmailList(email, activationLink) {
+    return {
         from: "Best Shop In ZATK",
         to: email,
         subject: "Email confirmation",
@@ -24,21 +28,24 @@ const timeOutVerification = (req, res) => {
                <div>Visit this link to confirm your account: ${activationLink}
         `
     }
+}
 
+function sendList(mailConfig) {
     mailer.sendMail(mailConfig, (err) => {
         if(err) {
-            console.log("Send Email error");
+            console.log(ERR_MAIL_DOESNT_SEND);
             return res.status(401).json({
-                status: "Error"
+                status: ERR_MAIL_DOESNT_SEND
             });
         } else {
-            console.log("Email send");
+            console.log(EMAIL_SUCCESS);
             return res.status(200).json({
-            status: "Email confirmation"
+            status: EMAIL_SUCCESS
             });
         }
     })
 }
+
 
 module.exports = {
     timeOutVerification
