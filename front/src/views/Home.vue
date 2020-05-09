7284f1c37f04691cdfad0d7dec74f7d06d7b3ad8
Alibriaan <dimulka62@gmail.com>
2020-05-09 13:15:33 +0300

    update (Home view):
    -update template
    -add route functionality

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,26 +1,35 @@
 <template>
-  <v-container class="home" fluid>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <v-row>
-      <v-col class="navigation-menu pa-1">
-        <v-list class="pa-0" v-if="catalog.length">
-          <v-subheader>Каталог</v-subheader>
-          <v-list-item v-for="(item,index) in catalog" @click="goToCatalog(item.name)" :key="index">
-           <v-list-item-content class="text-center">
-              {{ item.title }}
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-col>
-      <v-col class="main-shop-container">
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-container class="home-page d-flex align-center" fluid >
+      <v-container>
+      <v-row class="my-3 d-flex justify-center text-center">
+          <h1>Choose Your Destiny</h1>
+      </v-row>
+      <v-row class="d-flex justify-center">
+        <template v-for="(item,index) in catalog">
+          <v-btn v-if="item.status === 'active'" :key="index" class="ma-2" outlined @click="goToCatalogPage(item.name)">
+           {{item.title}}
+          </v-btn>
+          <v-btn disabled v-else :key="index" class="ma-2" outlined>{{item.title}} 
+            <v-badge
+            top
+            right
+            overlap
+            class="white--text"
+            color="green"
+            content="in dev"
+            offset-x="30"
+            offset-y="-8"
+            ></v-badge>
+          </v-btn>
+
+        </template>
+      </v-row>
+      </v-container>
+    </v-container>
 </template>
 
 <script>
 import getListOfItem from "../services/mainPage/getListOfItem.js";
-// import axios from "axios";
 export default {
   name: 'Home',
   data: () => {
@@ -28,7 +37,13 @@ export default {
       catalog: false
     }
   },
+  methods: {
+    goToCatalogPage(product) {
+        this.$router.push(`/catalog/${product}`);
+    }
+  },
   created() {
+      console.log("Get list of item");
       getListOfItem()
       .then((res) => {
           console.log(res);
@@ -39,20 +54,37 @@ export default {
           console.log(err);
           console.log("Error");
       });
-  },
+      console.log(this.$route)
+  }}
 </script>
+
 <style scoped>
 
-.navigation-menu{
-  background: red;
-  max-width: 250px;
+.home-page {
+  width: 100%;
+  height: 100%;
 }
 
-.main-shop-container{
-  background: green;
+a {
+  text-decoration: none;
 }
 
-#app .v-list-item {
-  min-height: 32px;
+
+.slide-left-enter-active {
+  animation: slide_left 0.5s;
+} 
+.slide-left-leave-active {
+  animation: slide_left 0.5s reverse;
+}
+
+@keyframes slide_left {
+    from{ 
+        transform: translateX(100px) scale(0.9);
+        opacity: 0;
+    }
+    to {
+        transform: translateX(0) scale(1);
+        opacity: 1;
+    }
 }
 </style>
