46c5f776a6e1b646fc83a99f4cc53e89b6f27753
Alibriaan <dimulka62@gmail.com>
2020-05-09 13:10:35 +0300

    update (vuetify config): add light and dark theme colors

--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -4,4 +4,35 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#3f51b5',
+            secondary: '#b0bec5',
+            accent: '#8c9eff',
+            error: '#ff5252',
+            primarylink: "#121212",
+            navigationlistprimary: "#ffffff",
+            navigationformtext: "#212121",
+            primaryrproductcard: "#ffffff",
+            primaryrproductcardtext: "#121212",
+            primaryregisrationauthorizationform: "#ffffff",
+            secondaryregisrationauthorizationform: "#1976D2",
+            paginationColor: "#1976D2"
+          },
+          dark: {
+              primary: "#E0E0E0",
+              secondary: "#424242",
+              primarylink: "#e0e0e0",
+              navigationlistprimary: "#E0E0E0",
+              navigationform: "#E0E0E0",
+              navigationformtext: "#212121",
+              primaryrproductcard: "#ffffff",
+              primaryrproductcardtext: "#121212",
+              primaryregisrationauthorizationform: "#ffffff",
+              secondaryregisrationauthorizationform: "#1976D2",
+              paginationColor: "#1976D2"
+          }
+        },
+      },
 });
