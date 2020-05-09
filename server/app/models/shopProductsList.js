c3f2975f6d884b16fcfb09d3239e225364b7e9e5
Alibriaan <dimulka62@gmail.com>
2020-05-09 13:21:55 +0300

    update (ShopProductList): mongodb model

--- server/app/models/shopProductsList.js
+++ server/app/models/shopProductsList.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const shopProductsList_schema = new Schema({
   name: String,
-  title: String
+  title: String,
+  status: String
 });
 
 mongoose.model("shopProductsList", shopProductsList_schema);
