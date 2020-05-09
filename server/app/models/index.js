999fb1267eeb40ee90a85a619f3e78ce4e12f822
Alibriaan <dimulka62@gmail.com>
2020-05-09 13:22:14 +0300

    update (index.js): mongo models import

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,11 +1,13 @@
 const article = require("./article");
 const registration = require("./registration");
 const prod_telephone = require("./products/telephone");
+const prod_computer = require("./products/computer");
 const shopProductsList = require("./shopProductsList");
 
 module.exports = {
     article,
     registration,
     prod_telephone,
+    prod_computer,
     shopProductsList
 };
