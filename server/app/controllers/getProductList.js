45f9edc25dc4285e9de3a1331386ec342a7a3bc8
Alibriaan <dimulka62@gmail.com>
2020-05-09 13:19:04 +0300

    update ( getProductList controller):  refactoring

--- server/app/controllers/getProductList.js
+++ server/app/controllers/getProductList.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+let shopItemList = require("../../config/data.js").shopItemsList;
+
 const shopProductsList = mongoose.model("shopProductsList");
 
 const GET_LIST_ERROR = "GET_LIST_ERROR";
 const ERR_EMPTY_LIST = "ERR_EMPTY_LIST";
 const SUCCES_GET_LIST_OF_ITEMS = "SUCCES_GET_LIST_OF_ITEMS";
+const SERVER_GET_DATA_ERROR = "SERVER_GET_DATA_ERROR";
+const SUCCESS_SEND = "SUCCESS_SEND";
 
 const getAllShopProducts = (req, res) => {
     console.log("Get all producst");
@@ -29,6 +33,26 @@ const getAllShopProducts = (req, res) => {
     })
     .catch(err => res.status(500).json({message: err.message}));
 }
+
+
+const getProductList = (req, res) => { 
+    console.log("product list");
+    console.log(req.body);
+    const list = shopItemList[req.body.type];
+    console.log(list);
+    if(list.length) {
+        res.send({
+            status: SUCCESS_SEND,
+            payload: list
+         })
+    } else {
+        res.status(401).json({
+            status: SERVER_GET_DATA_ERROR
+        });
+    }
+}
+
 module.exports = {
-    getAllShopProducts
+    getAllShopProducts,
+    getProductList
 }
