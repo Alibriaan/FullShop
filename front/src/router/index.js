bf5d24a220d86690164809efe1821a1d1c2f1987
Alibriaan <dimulka62@gmail.com>
2020-05-09 13:13:03 +0300

    update (Vue router):
    -add catalog
    -add basket
    -add metadata

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Catalog from '../views/Catalog.vue'
+import Basket from '../views/Basket.vue';
+
 import authorizationRegistration from "../views/authorizationRegistration.vue";
 import verificationPage from "../views/Confirmation.vue";
 
@@ -10,26 +13,48 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      headerDisplay: false,
+      logoutButtonDisplay: true
+    } 
   },
   {
     path: "/authorization-registration",
     name: "Authorization-Registration",
-    component: authorizationRegistration
+    component: authorizationRegistration,
+    meta: {
+      headerDisplay: false,
+      logoutButtonDisplay: false
+    } 
   },
   {
     path: "/confirmation/:verificationToken",
     name: "Confirmation page",
-    component: verificationPage
+    component: verificationPage,
+    meta: {
+      headerDisplay: false,
+      logoutButtonDisplay: false
+    } 
   },
   {
-    path: '/posts',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: "/catalog/:id",
+    name: "Catalog",
+    component: Catalog,
+    meta: {
+      headerDisplay: false,
+      logoutButtonDisplay: true
+    },
+  },
+  {
+    path: "/basket",
+    name: "Basket",
+    component: Basket,
+    meta: {
+      headerDisplay: false,
+      logoutButtonDisplay: true
+    },
+  },
 ]
 
 const router = new VueRouter({
